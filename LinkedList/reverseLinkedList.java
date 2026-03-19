13ce2d84cbf9a80e8625333e384be771acc2da45
AVANISH VERMA <avanishv708@gmail.com>
2026-03-19 20:16:22 +0530

    Create reverseLinkedList.java
    Reverse Linked List (LeetCode 206)

---
+++ LinkedList/reverseLinkedList.java
@@ -0,0 +1,20 @@
+// Problem: Reverse Linked List
+// LeetCode: 206
+// Approach: Iterative
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

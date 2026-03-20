96aad1fd607ed1e545cb9b72b56e091a7716f9ca
AVANISH VERMA <avanishv708@gmail.com>
2026-03-20 14:05:36 +0530

    Create DeleteNode.java

---
+++ LinkedList/DeleteNode.java
@@ -0,0 +1,10 @@
+// LeetCode Problem: Delete Node in a Linked List(237)
+// Approach: Copy next node value and skip it
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
